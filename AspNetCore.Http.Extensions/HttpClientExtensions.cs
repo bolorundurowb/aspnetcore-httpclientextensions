7078e2a7eaa72375eddfb3b1962e81f29c245a62
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-07-15 23:58:24 +0300

    ft: upgrade from newtonsoft to system.text

--- AspNetCore.Http.Extensions/HttpClientExtensions.cs
+++ AspNetCore.Http.Extensions/HttpClientExtensions.cs
@@ -1,34 +1,33 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
-namespace AspNetCore.Http.Extensions
+namespace AspNetCore.Http.Extensions;
+
+public static class HttpClientExtensions
 {
-    public static class HttpClientExtensions
+    public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
+        this HttpClient httpClient, string url, T data)
     {
-        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
-            this HttpClient httpClient, string url, T data)
-        {
-            var dataAsString = JsonConvert.SerializeObject(data);
-            var content = new StringContent(dataAsString);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            return httpClient.PostAsync(url, content);
-        }
+        var dataAsString = JsonSerializer.Serialize(data);
+        var content = new StringContent(dataAsString);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        return httpClient.PostAsync(url, content);
+    }
         
-        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
-            this HttpClient httpClient, string url, T data)
-        {
-            var dataAsString = JsonConvert.SerializeObject(data);
-            var content = new StringContent(dataAsString);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            return httpClient.PutAsync(url, content);
-        }
+    public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
+        this HttpClient httpClient, string url, T data)
+    {
+        var dataAsString = JsonSerializer.Serialize(data);
+        var content = new StringContent(dataAsString);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        return httpClient.PutAsync(url, content);
+    }
 
-        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
-        {
-            var dataAsString = await content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<T>(dataAsString);
-        }
+    public static async Task<T?> ReadAsJsonAsync<T>(this HttpContent content)
+    {
+        var dataAsString = await content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<T>(dataAsString);
     }
 }
