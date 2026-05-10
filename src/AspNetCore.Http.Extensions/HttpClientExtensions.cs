55cfdfec6a7df61779f76f0cb700ab2a2ccd99c5
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-05-10 22:27:11 +0100

    ch: new syntax for extensions and  fix readme packaging issues

--- src/AspNetCore.Http.Extensions/HttpClientExtensions.cs
+++ src/AspNetCore.Http.Extensions/HttpClientExtensions.cs
@@ -7,22 +7,23 @@ namespace AspNetCore.Http.Extensions;
 
 public static class HttpClientExtensions
 {
-    public static Task<HttpResponseMessage> PostAsJsonAsync<T>(
-        this HttpClient httpClient, string url, T data)
+    extension(HttpClient httpClient)
     {
-        var dataAsString = JsonSerializer.Serialize(data);
-        var content = new StringContent(dataAsString);
-        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        return httpClient.PostAsync(url, content);
-    }
+        public Task<HttpResponseMessage> PostAsJsonAsync<T>(string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PostAsync(url, content);
+        }
 
-    public static Task<HttpResponseMessage> PutAsJsonAsync<T>(
-        this HttpClient httpClient, string url, T data)
-    {
-        var dataAsString = JsonSerializer.Serialize(data);
-        var content = new StringContent(dataAsString);
-        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-        return httpClient.PutAsync(url, content);
+        public Task<HttpResponseMessage> PutAsJsonAsync<T>(string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PutAsync(url, content);
+        }
     }
 
     public static async Task<T?> ReadAsJsonAsync<T>(this HttpContent content)
